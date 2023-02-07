1c7182bd31a7d2440acf7d1a8d82a8f55c0c5d5f
finnless <nolan@nolanwidham.com>
2023-02-06 17:35:35 -0800

    fix HTML_Validator syntax for linter

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -3,7 +3,8 @@
 
 def validate_html(html):
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
+    This function performs a limited version of html validation by
+    checking whether every opening tag has a corresponding closing tag.
 
     >>> validate_html('<strong>example</strong>')
     True
@@ -16,7 +17,7 @@ def validate_html(html):
 
     tags = _extract_tags(html)
     stack = []
-    
+
     # catch if no tags
     if len(tags) == 0:
         return False
@@ -35,19 +36,24 @@ def validate_html(html):
     return len(stack) == 0
 
     # HINT:
-    # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the class/book
-    # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
+    # use the _extract_tags function below to generate a list of
+    # html tags without any extra text;
+    # then process these html tags using the balanced parentheses
+    # algorithm from the class/book
+    # the main difference between your code and the code from class
+    # will be that you will have to keep track of not just the 3
+    # types of parentheses,
     # but arbitrary text located between the html tags
-
 
 
 def _extract_tags(html):
     '''
     This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
+    By convention in Python, helper functions that are not meant to
+    be used directly by the user are prefixed with an underscore.
 
-    This function returns a list of all the html tags contained in the input string,
+    This function returns a list of all the html tags contained in
+    the input string,
     stripping out all text not contained within angle brackets.
 
     >>> _extract_tags('Python <strong>rocks</strong>!')
@@ -56,7 +62,7 @@ def _extract_tags(html):
     in_tag = False
     tag = ""
     output = []
-    
+
     for c in html:
         if c == '<' and not in_tag:
             in_tag = True
